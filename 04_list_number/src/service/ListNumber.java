211f12b525f2025b67ec1e14c426a94ff0d99498
manana <manana@host.docker.internal>
2024-04-11 12:16:12 +0200

    Creación clase Test. Porbar clase ListNumber

--- 04_list_number/src/service/ListNumber.java
+++ 04_list_number/src/service/ListNumber.java
@@ -1,4 +1,4 @@
-package principal;
+package service;
 
 import java.util.ArrayList;
 
@@ -55,12 +55,6 @@ public class ListNumber extends ArrayList<Integer>{
 			return super.add(e);
 		}
 		return false;
-	}
-
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
