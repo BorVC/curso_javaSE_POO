bfabb1323c76676cb0152c1ef005fe9339578692
borja <borjavidalcormenzana@gmail.com>
2024-04-21 20:22:36 +0200

    Proyecto pruebas. Interface Stream

---
+++ pruebas/src/pruebas/Pruebas1.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import java.util.stream.Stream;
+
+public class Pruebas1 {
+
+	public static void main(String[] args) {
+		Stream <Integer>st = Stream.of(2,3,6,5,4,10,13,7,9);
+		//st.forEach(n -> System.out.println(n));
+		//System.out.println(st.count());
+		//System.out.println(st.filter(n -> n < 10).count());
+
+		
+	}
+
+}
